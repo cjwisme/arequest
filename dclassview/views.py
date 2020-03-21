a36906154dfa2e024f2d332d25c20b5018747cad
cjw <cjwisme@aliyun.com>
2020-03-21 13:20:35 +0800

    1.定义装饰 2.使用装饰器的3中方式 2.1 修改自定义的装饰器,嵌套方式添加参数 2.2 路由包裹 2.3 调用django 自带的方法,method_decorator

--- dclassview/views.py
+++ dclassview/views.py
@@ -1,14 +1,35 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
 from django.views.generic.base import View
 
 # Create your views here.
 
-# 视图函数
 
+# 装饰器
 
+def login_decorator(func):
+    def wrapper(self,request,*args,**kwargs):
+        print("自定义装饰器")
+        return func(self,request,*args,**kwargs)
+    return wrapper
 
+# 1.方式一 修改自定义装饰器
+
+# 2.装饰在路由 path("dclassview",login_decorator(views.LoginView.as_view())),
+
+# 3.调用django 自带的装饰方法 method_decorator()
+def django_login_decorator(func):
+    def wrapper(request,*args,**kwargs):
+        print("自定义装饰器")
+        return func(request,*args,**kwargs)
+    return wrapper
+#类视图
+# @method_decorator(django_login_decorator,name="get")
+@method_decorator(django_login_decorator,name="dispatch")
 class LoginView(View):
+    # @login_decorator
+    @method_decorator(django_login_decorator)
     def get(self,request):
         return HttpResponse("类视图----GET---登录页面")
     
@@ -16,6 +37,8 @@ class LoginView(View):
         return HttpResponse("类视图----POST---登录页面")
 
 
+
+# 视图函数
 def index(request):
     if request.method == "GET":
         return HttpResponse("视图函数----GET---登录页面")
