d59d1afa692fd6cf1272db470cd9ed5eb364b872
cjw <cjwisme@aliyun.com>
2020-03-22 12:32:22 +0800

    1.model 增删改 1.1增  create() obj.save() 1.2删  obj.delete()  filter().delete() 1.3改  obj.attr   update()

--- emodels/views.py
+++ emodels/views.py
@@ -1,3 +1,33 @@
+from datetime import date
 from django.shortcuts import render
 
 # Create your views here.
+
+def models_operator():
+    from emodels.models import BookInfo
+    # 添加
+    # 1.方式一
+    BookInfo.objects.create(
+        btitle="西游记",
+        bpub_date=date(1995,1,1)
+    )
+    # 2.方式二
+    book = BookInfo(        
+        btitle="我不",
+        bpub_date=date(1995,1,1)
+    )
+    book.save()
+    
+    # 删除
+    # 方式一
+    book = BookInfo.objects.get(id=5)
+    book.delete()
+    # 方式二
+    BookInfo.objects.filter(id=6).delete()
+    
+    # 更新
+    # 方式一
+    book = BookInfo.objects.get(id=4)
+    book.bread = 30
+    # 方式二
+    BookInfo.objects.update(bread=30)
